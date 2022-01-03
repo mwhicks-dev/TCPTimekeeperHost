8ff481fcd27fb7a9014a5e7906838a00da45c3d5
abstractionalpha <abstractionalpha@abstractionAlpha>
2022-01-03 14:46:28 -0500

    [Implementation] Rewrote method/state skeletons for Host

--- TCPTimekeeperHost/src/main/java/com/abstractionalpha/network/tcp/timekeeper/host/Host.java
+++ TCPTimekeeperHost/src/main/java/com/abstractionalpha/network/tcp/timekeeper/host/Host.java
@@ -1,3 +1,45 @@
 package com.abstractionalpha.network.tcp.timekeeper.host;
 
-public class Host {}
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class Host {
+	
+	/** Socket for communication with client */
+	private Socket socket = null;
+	
+	/** Input stream for socket communication */
+	private DataInputStream input = null;
+	
+	/** Output stream for socket communication */
+	private DataOutputStream output = null;
+	
+	/**
+	 * Handles Client instances accepted in Host#run.
+	 * 
+	 * @param socket
+	 */
+	private void handleClient(Socket socket) {
+		// TODO Implement method
+	}
+	
+	/**
+	 * Accepts Client instances.
+	 * 
+	 * @param args -- command-line arguments
+	 */
+	private void run(String[] args) {
+		// TODO Implement method
+	}
+	
+	/**
+	 * Main method. Initializes and runs a Host instance.
+	 * 
+	 * @param args -- command-line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Implement method
+	}
+	
+}
