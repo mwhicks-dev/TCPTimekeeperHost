c7bd2441f12c678efc6b12d6c83d9ccbb590dfeb
Mason <Mason@DESKTOP-NP6OR4H>
2022-01-02 23:22:47 -0500

    [Implementation] Wrote Host#getMessage, debugged Host#run

--- TCPTimekeeperHost/src/main/java/com/abstractionalpha/network/tcp/timekeeper/host/Host.java
+++ TCPTimekeeperHost/src/main/java/com/abstractionalpha/network/tcp/timekeeper/host/Host.java
@@ -1,5 +1,7 @@
 package com.abstractionalpha.network.tcp.timekeeper.host;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,8 +12,57 @@ public class Host {
 	
 	public static final String TRASNFORM_STRING = "AES/ECB/PKCS5Padding";
 	
+	private byte[] getMessage(DataInputStream input) throws IOException {
+		int len = input.readInt();
+		byte[] msg = new byte[len];
+		input.readFully(msg);
+		return msg;
+	}
+	
+	private void putMessage(DataOutputStream output, byte[] msg) throws IOException {
+		// Write length of given message followed by its contents
+		output.writeInt(msg.length);
+		output.write(msg, 0, msg.length);
+		output.flush();
+	}
+	
 	private void handleClient(Socket socket) {
-		// TODO implement method
+		try {
+			// Get socket IO streams
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+			
+			// Get first client command
+			String request = new String( getMessage( input ) );
+			
+			// Process request
+			while (!request.equals("quit")) {
+				// Initialize StringBuilder
+				StringBuilder reply = new StringBuilder();
+				// String invalid = "Invalid command\n";
+				
+				// Process command
+				// TODO add actual command handling
+				
+				// Build reply string
+				reply.append(request);
+				reply.append('\n');
+				
+				// Send reply and get next command
+				putMessage( output, reply.toString().getBytes() );
+				request = new String( getMessage( input ) );
+			}
+		} catch (IOException ioe) {
+			System.err.println(String.format("IO Error: %s", ioe));
+		} catch (Exception e) {
+			System.err.println(String.format("General Error: %s", e));
+		} finally {
+			try {
+				socket.close();
+			} catch (Exception e) {
+				// Do nothing
+			}
+		}
 	}
 	
 	private void run(String[] args) {
@@ -30,7 +81,7 @@ public class Host {
 		}
 		
 		// Accept and read new connections
-		while (true) {
+		/*while (true) {
 			try {
 				// Accept new client connection
 				Socket sock = serverSocket.accept();
@@ -44,6 +95,28 @@ public class Host {
 				t.start();
 			} catch (IOException e) {
 				System.err.println(String.format("Can't accept client %s", e));
+			}
+		}*/
+		try {
+			while (true) {
+				// Accept new client connection
+				final Socket sock = serverSocket.accept();
+				
+				// Handle client
+				Thread t = new Thread() {
+					public void run() {
+						handleClient(sock);
+					}
+				};
+				t.start();
+			}
+		} catch (IOException ioe) {
+			System.err.println(String.format("Can't accept client %s", ioe));
+		} finally {
+			try {
+				serverSocket.close();
+			} catch (IOException e) {
+				// Do nothing
 			}
 		}
 	}
