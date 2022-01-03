1371db526172bc0ec3fd559e1b7b70f4c840c035
Mason <Mason@DESKTOP-NP6OR4H>
2022-01-02 22:57:39 -0500

    [Implementation] Added state skeleton to Host

--- TCPTimekeeperHost/src/main/java/com/abstractionalpha/network/tcp/timekeeper/host/Host.java
+++ TCPTimekeeperHost/src/main/java/com/abstractionalpha/network/tcp/timekeeper/host/Host.java
@@ -1,5 +1,23 @@
 package com.abstractionalpha.network.tcp.timekeeper.host;
 
+import java.net.Socket;
+
 public class Host {
+	
+	public static final int PORT_NUMBER = 25600;
+	
+	public static final String TRASNFORM_STRING = "AES/ECB/PKCS5Padding";
+	
+	private void handleClient(Socket socket) {
+		// TODO implement method
+	}
+	
+	private void run(String[] args) {
+		// TODO implement method
+	}
+	
+	public static void main(String[] args) {
+		// TODO implement method
+	}
 
 }
